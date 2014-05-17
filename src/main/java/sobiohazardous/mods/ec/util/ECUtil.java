38fa6b9c5470bc5b479df3ea45343e1537d53ff1
Clashsoft <clash2@hotmail.de>
2014-05-17 20:59:09 +0200

    Implemented melting

--- src/main/java/sobiohazardous/mods/ec/util/ECUtil.java
+++ src/main/java/sobiohazardous/mods/ec/util/ECUtil.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockBush;
 import net.minecraft.block.BlockLeavesBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class ECUtil
 {
@@ -81,8 +82,90 @@ public class ECUtil
 	
 	public static boolean melt(World world, int x, int y, int z, boolean flag)
 	{
+		Block block = world.getBlock(x, y, z);
+		
+		if (block.getFlammability(world, x, y, z, ForgeDirection.NORTH) > 1)
+		{
+			world.setBlock(x, y, z, Blocks.fire);
+			flag = true;
+		}
+		else if (block == Blocks.stone || block == Blocks.end_stone)
+		{
+			world.setBlock(x, y, z, ECBlocks.moltenstone, 1, 3);
+			flag = true;
+		}
+		else if (block == ECBlocks.moltenstone)
+		{
+			if (world.getBlockMetadata(x, y, z) == 1)
+			{
+				world.setBlock(x, y, z, ECBlocks.moltenstone);
+				flag = true;
+			}
+		}
+		else if (block == Blocks.cobblestone || block == Blocks.mossy_cobblestone || block == Blocks.stonebrick || block == Blocks.gravel)
+		{
+			world.setBlock(x, y, z, Blocks.stone);
+			flag = true;
+		}
+		else if (block == Blocks.sandstone)
+		{
+			if (world.getBlockMetadata(x, y, z) != 1)
+			{
+				world.setBlockMetadataWithNotify(x, y, z, 1, 3);
+			}
+			else
+			{
+				world.setBlock(x, y, z, Blocks.flowing_lava);
+			}
+			flag = true;
+		}
+		else if (block == Blocks.cobblestone_wall)
+		{
+			world.setBlock(x, y, z, Blocks.flowing_lava);
+			flag = true;
+		}
+		else if (block == Blocks.farmland || block == Blocks.grass || block == Blocks.grass)
+		{
+			world.setBlock(x, y, z, Blocks.dirt);
+			flag = true;
+		}
+		else if (block == Blocks.dirt)
+		{
+			if (world.getBlockMetadata(x, y, z) != 0)
+			{
+				world.setBlockMetadataWithNotify(x, y, z, 0, 3);
+				flag = true;
+			}
+		}
+		else if (block == Blocks.sand)
+		{
+			world.setBlock(x, y, z, Blocks.glass);
+			flag = true;
+		}
+		else if (block == Blocks.obsidian || block == Blocks.netherrack)
+		{
+			world.setBlock(x, y, z, Blocks.lava);
+			flag = true;
+		}
+		else if (block == Blocks.packed_ice)
+		{
+			world.setBlock(x, y, z, Blocks.ice);
+			flag = true;
+		}
+		else if (block == Blocks.ice)
+		{
+			world.setBlock(x, y, z, Blocks.snow);
+			flag = true;
+		}
+		else if (block == Blocks.snow_layer)
+		{
+			world.setBlockToAir(x, y, z);
+			flag = true;
+		}
+		
 		if (flag)
 		{
+			world.playAuxSFX(2004, x, y, z, 0);
 			world.playAuxSFX(2004, x, y, z, 0);
 		}
 		
