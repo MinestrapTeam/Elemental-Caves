eed12f11ab3bbfde825a5c5f9dd47a5a42f048ec
Clashsoft <clash2@hotmail.de>
2014-05-18 02:16:23 +0200

    Fixed Gems and Shards not showing up on the creative tab

--- src/main/java/sobiohazardous/mods/ec/item/ECItemMulti.java
+++ src/main/java/sobiohazardous/mods/ec/item/ECItemMulti.java
@@ -1,19 +1,23 @@
 package sobiohazardous.mods.ec.item;
 
+import java.util.List;
+
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 
-public class ECItemMulti extends Item
+public class ECItemMulti extends ECItem
 {
-	public String[] types;
+	public String[]	types;
 	
-	public IIcon[] icons;
+	public IIcon[]	icons;
 	
 	public ECItemMulti(String[] types)
 	{
 		this.types = types;
+		this.setHasSubtypes(true);
 	}
 	
 	@Override
@@ -42,5 +46,14 @@ public class ECItemMulti extends Item
 		if (metadata < 0 || metadata >= this.icons.length)
 			metadata = 0;
 		return this.icons[metadata];
+	}
+	
+	@Override
+	public void getSubItems(Item item, CreativeTabs tab, List list)
+	{
+		for (int i = 0; i < this.types.length; i++)
+		{
+			list.add(new ItemStack(this, 1, i));
+		}
 	}
 }
