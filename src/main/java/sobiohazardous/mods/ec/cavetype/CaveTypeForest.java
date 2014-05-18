24c0f01a79510719891682ecb5fa12b39b8e37e5
Clashsoft <clash2@hotmail.de>
2014-05-18 20:56:11 +0200

    Improved Forst Caves

--- src/main/java/sobiohazardous/mods/ec/cavetype/CaveTypeForest.java
+++ src/main/java/sobiohazardous/mods/ec/cavetype/CaveTypeForest.java
@@ -2,7 +2,6 @@ package sobiohazardous.mods.ec.cavetype;
 
 import java.util.Random;
 
-import sobiohazardous.mods.ec.block.BlockRichGrass;
 import sobiohazardous.mods.ec.lib.ECBlocks;
 
 import net.minecraft.init.Blocks;
@@ -10,7 +9,6 @@ import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.BiomeGenForest;
 import net.minecraft.world.biome.BiomeGenJungle;
-import net.minecraft.world.gen.feature.WorldGenTrees;
 
 public class CaveTypeForest extends CaveType
 {
@@ -21,8 +19,6 @@ public class CaveTypeForest extends CaveType
 		this.getWallGen().addReplacement(Blocks.dirt, ECBlocks.richSoil);
 		this.getWallGen().addReplacement(Blocks.gravel, ECBlocks.richSoil);
 		
-		this.setFloorAddonSpawnWeight(0.5F);
-		
 		// TODO Add ores
 		// TODO add more flowers
 	}
@@ -30,10 +26,8 @@ public class CaveTypeForest extends CaveType
 	@Override
 	public void generateFloorAddons(World world, Random random, int x, int y, int z)
 	{
-		((BlockRichGrass) ECBlocks.richGrass).func_149853_b(world, random, x, y, z);
-		
-		if (random.nextFloat() < 0.01F)
-			new WorldGenTrees(false).generate(world, random, x, y, z);
+		if (random.nextInt(10) == 0)
+			world.setBlock(x, y, z, Blocks.sapling);
 	}
 	
 	@Override
