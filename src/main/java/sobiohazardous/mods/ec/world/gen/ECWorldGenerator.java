749a0a58a4bffff1f647c0d1b407afc2350e6e9e
Clashsoft <clash2@hotmail.de>
2014-05-18 23:08:28 +0200

    Supercharged cave generator

--- src/main/java/sobiohazardous/mods/ec/world/gen/ECWorldGenerator.java
+++ src/main/java/sobiohazardous/mods/ec/world/gen/ECWorldGenerator.java
@@ -36,46 +36,20 @@ public class ECWorldGenerator implements IWorldGenerator
 		}
 	}
 	
-	public void generateSurface(World world, Random rand, int chunkX, int chunkZ, IChunkProvider chunk)
-	{
-		this.replaceCaves(world, rand, chunkX, chunkZ);
-	}
-	
-	public void generateNether(World world, Random rand, int chunkX, int chunkZ)
-	{
-		WorldGenMinable glisteningOreGen = new WorldGenMinable(ECBlocks.oreGlistening, 5, Blocks.netherrack);
-		
-		int x1;
-		int y1;
-		int z1;
-		
-		for (int i = 0; i < 10; i++)
-		{
-			x1 = chunkX + rand.nextInt(16);
-			y1 = rand.nextInt(40);
-			z1 = chunkZ + rand.nextInt(16);
-			
-			glisteningOreGen.generate(world, rand, x1, y1, z1);
-		}
-	}
-	
-	public void generateEnd(World world, Random rand, int chunkX, int chunkZ)
-	{
-	}
-	
-	public void replaceCaves(World world, Random rand, int chunkX, int chunkZ)
+	public static void generateSurface(World world, Random rand, int chunkX, int chunkZ, IChunkProvider chunk)
 	{
 		for (int x0 = 0; x0 < 16; x0++)
 		{
+			int x1 = chunkX + x0;
 			for (int z0 = 0; z0 < 16; z0++)
 			{
-				int x1 = chunkX + x0;
 				int z1 = chunkZ + z0;
+				
 				for (CaveType type : CaveType.caveTypes)
 				{
 					if (type.canGenerateAt(world, x1, z1))
 					{
-						type.generate(world, rand, x1, z1);
+						replaceCaves(type, world, rand, x1, z1);
 					}
 				}
 			}
@@ -97,6 +71,58 @@ public class ECWorldGenerator implements IWorldGenerator
 					ore.generate(world, rand, x1, y1, z1);
 				}
 			}
+		}
+	}
+	
+	public static void generateNether(World world, Random rand, int chunkX, int chunkZ)
+	{
+		WorldGenMinable glisteningOreGen = new WorldGenMinable(ECBlocks.oreGlistening, 5, Blocks.netherrack);
+		
+		int x1;
+		int y1;
+		int z1;
+		
+		for (int i = 0; i < 10; i++)
+		{
+			x1 = chunkX + rand.nextInt(16);
+			y1 = rand.nextInt(40);
+			z1 = chunkZ + rand.nextInt(16);
+			
+			glisteningOreGen.generate(world, rand, x1, y1, z1);
+		}
+	}
+	
+	public static void generateEnd(World world, Random rand, int chunkX, int chunkZ)
+	{
+	}
+	
+	public static void replaceCaves(CaveType type, World world, Random random, int x, int z)
+	{
+		int y = type.spawnHeight;
+		
+		int ceiling = -1;
+		boolean wasAir = false;
+		while (y > 5)
+		{
+			if (world.isAirBlock(x, y, z))
+			{
+				if (!wasAir && ceiling == -1)
+				{
+					ceiling = y + 1;
+				}
+				wasAir = true;
+			}
+			else
+			{
+				if (ceiling != -1)
+				{
+					type.generate(world, random, x, y, ceiling, z);
+					ceiling = -1;
+					y -= 4;
+				}
+				wasAir = false;
+			}
+			y--;
 		}
 	}
 }
