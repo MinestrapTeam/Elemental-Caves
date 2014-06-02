3b223586e04b85682aec74b8810b762d995a2c62
Clashsoft <clashsoft@hotmail.com>
2014-06-02 23:09:20 +0200

    1.7.2-1.1.0

--- src/main/java/minestrapteam/elementalcaves/lib/ECReference.java
+++ src/main/java/minestrapteam/elementalcaves/lib/ECReference.java
@@ -4,5 +4,5 @@ public class ECReference
 {
 	public static final String	MODID	= "ElementalCaves";
 	public static final String	NAME	= "Elemental Caves";
-	public static final String	VERSION	= "1.7.2-1.0.0";
+	public static final String	VERSION	= "1.7.2-1.1.0";
 }
