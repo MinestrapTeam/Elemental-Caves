f481c50b0a5a4ba41436c6d65e70206a4af7d1cd
Domenic Portuesi <sobiohazardous@gmail.com>
2014-06-12 16:20:59 -0400

    1.7.2-1.1.3
    * Made ticks to update crystals configurable

--- src/main/java/minestrapteam/elementalcaves/lib/ECConfig.java
+++ src/main/java/minestrapteam/elementalcaves/lib/ECConfig.java
@@ -5,12 +5,14 @@ import net.minecraftforge.common.config.Configuration;
 public class ECConfig
 {
 	public static boolean	inferniumArmorEffect	= false;
+	public static int		ticksToUpdateCrystals = 100;
 	
 	public static void init(Configuration config)
 	{
 		config.load();
 		
 		config.get("Misc", "Infernium Armor Fire Effect", inferniumArmorEffect);
+		config.get("Misc", "Ticks to update crystals", ticksToUpdateCrystals);
 		
 		config.save();
 	}
