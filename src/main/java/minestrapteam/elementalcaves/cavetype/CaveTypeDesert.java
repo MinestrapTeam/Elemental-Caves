60ee7bbb420a7547dc3fdd0941b0a6da8f489bd0
Domenic Portuesi <sobiohazardous@gmail.com>
2014-06-20 21:44:40 -0400

    Added generation settings for desert caves

--- src/main/java/minestrapteam/elementalcaves/cavetype/CaveTypeDesert.java
+++ src/main/java/minestrapteam/elementalcaves/cavetype/CaveTypeDesert.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 import minestrapteam.caveapi.cavetype.CaveType;
 import minestrapteam.elementalcaves.lib.ECBlocks;
  
@@ -24,4 +25,9 @@ public class CaveTypeDesert extends CaveType
 		world.setBlock(x, y, z, ECBlocks.sand, 0, 3);
 	}
 	
+	@Override
+	public boolean canGenerateInBiome(BiomeGenBase biome)
+	{
+		return biome == biome.desert || biome == biome.desertHills || biome == biome.mesa || biome == biome.mesaPlateau || biome == biome.mesaPlateau_F;
+	}
 }
