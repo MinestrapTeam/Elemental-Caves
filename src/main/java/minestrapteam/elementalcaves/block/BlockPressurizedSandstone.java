e0a845f47aa86a8fd1eb43b67a4a287c9b263070
Clashsoft <clashsoft@hotmail.com>
2014-07-28 20:47:51 +0200

    Added Gilded Pressurized Sandstone

--- src/main/java/minestrapteam/elementalcaves/block/BlockPressurizedSandstone.java
+++ src/main/java/minestrapteam/elementalcaves/block/BlockPressurizedSandstone.java
@@ -13,31 +13,36 @@ import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 
 /**
- * All pressurized sandstone types. 
+ * All pressurized sandstone types.
  * <ul>
  * <li>Meta 0: Normal
  * <li>Meta 1: Chiseled
  * <li>Meta 2: Smooth
  * <li>Meta 3: Hieroglyph
- * <li>Meta 4: Hieroglyph2
+ * <li>Meta 4: Hieroglyph 2
  * <li>Meta 5: Bejeweled
- * <li>Meta 6: Bejeweled2
+ * <li>Meta 6: Bejeweled 2
+ * <li>Meta 7: Gilded
+ * <li>Meta 8: Gilded 2
  * </ul>
+ * 
  * @author SoBiohazardous
- *
+ * @author Clashsoft
  */
 public class BlockPressurizedSandstone extends ECBlock
 {
-	public IIcon bottomIcon;
-	public IIcon sideIcon;
-	public IIcon sideIcon2;
-	public IIcon chiseledIcon;
-	public IIcon smoothIcon;
-	public IIcon hieroglyphIcon;
-	public IIcon hieroglyphIcon2;
-	public IIcon bejeweledIcon;
-	public IIcon bejeweledIcon2;
-
+	public IIcon	bottomIcon;
+	public IIcon	sideIcon;
+	public IIcon	sideIcon2;
+	public IIcon	chiseledIcon;
+	public IIcon	smoothIcon;
+	public IIcon	hieroglyphIcon;
+	public IIcon	hieroglyphIcon2;
+	public IIcon	bejeweledIcon;
+	public IIcon	bejeweledIcon2;
+	public IIcon	gildedIcon;
+	public IIcon	gildedIcon2;
+	
 	public BlockPressurizedSandstone(Material material)
 	{
 		super(material);
@@ -55,20 +60,22 @@ public class BlockPressurizedSandstone extends ECBlock
 		this.hieroglyphIcon = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_hieroglyph_1"));
 		this.hieroglyphIcon2 = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_hieroglyph_2"));
 		this.bejeweledIcon = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_bejeweled_1"));
-		this.bejeweledIcon2 = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_bejeweled_1"));
+		this.bejeweledIcon2 = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_bejeweled_2"));
+		this.gildedIcon = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_gilded_1"));
+		this.gildedIcon2 = iconRegister.registerIcon(ECUtil.getTexture("pressurized_sandstone_gilded_2"));
 	}
 	
 	@Override
 	public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side)
 	{
 		if (side == 0)
-    	{
-    		return this.bottomIcon;
-    	}
-    	else if (side == 1)
-    	{
-    		return this.blockIcon;
-    	}
+		{
+			return this.bottomIcon;
+		}
+		else if (side == 1)
+		{
+			return this.blockIcon;
+		}
 		
 		int metadata = world.getBlockMetadata(x, y, z);
 		if (metadata == 0)
@@ -103,18 +110,23 @@ public class BlockPressurizedSandstone extends ECBlock
 		{
 			return this.bejeweledIcon2;
 		}
+		else if (metadata == 7)
+		{
+			return this.gildedIcon;
+		}
+		else if (metadata == 8)
+		{
+			return this.gildedIcon2;
+		}
 		return this.blockIcon;
 	}
-    
-    @Override
-    public void getSubBlocks(Item item, CreativeTabs tab, List list)
-    {
-    	list.add(new ItemStack(item, 1, 0));
-    	list.add(new ItemStack(item, 1, 1));
-    	list.add(new ItemStack(item, 1, 2));
-    	list.add(new ItemStack(item, 1, 3));
-    	list.add(new ItemStack(item, 1, 4));
-    	list.add(new ItemStack(item, 1, 5));
-    	list.add(new ItemStack(item, 1, 6));
-    }
+	
+	@Override
+	public void getSubBlocks(Item item, CreativeTabs tab, List list)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			list.add(new ItemStack(item, 1, i));
+		}
+	}
 }
